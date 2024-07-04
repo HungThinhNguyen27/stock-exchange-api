005f4d33f5bbf581d817c3390308fdd8be12ae36
HungThinhNguyen27 <Thung4199@gmail.com>
2024-07-04 11:01:04 +0700

    update query function stock_candles

--- src/data_layer/table/stock.py
+++ src/data_layer/table/stock.py
@@ -44,6 +44,29 @@ class StockPriceDL(MySqlConnect):
         return [ts[0] for ts in limited_time_stamps]
 
     def get_by_period_and_limit(self, period, limit, page): 
+
+        
+        """
+        - Noted : 1 period corresponds to 1 minute
+        The function first calculates whether the period the user wants to query is hours or days 
+        If greater than or equal to 1440, it means the user wants to access the date and less than 1440 will access the time.The function will then calculate time 
+
+            - For example, the user uses period 2880 (ie 2 days) and limit is 70 records:
+                time = (2880 // 1440) * 70 = 140 days
+                and will calculate from the current date to 140 days ago as well as returns 70 records for 2 days each.
+
+        Retrieves aggregated stock price data based on a specified time period and limit,
+        ordered by trade date in descending order.
+
+        Args:
+            period (int): The time period in minutes for aggregation.
+            limit (int): The maximum number of records to retrieve.
+            page (int): The page number for pagination (not implemented in current function).
+
+        Returns:
+            list: A list of tuples containing aggregated stock price data, including trade date,
+                open price, close price, high price, low price, and volume.
+        """
         if period >= 1440: 
             str = "DAY"
             time = (period // 1440) * limit
